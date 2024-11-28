99d76164212496b36ed7eb5509f0c8c5181dd995
Suresh Kondepudi <nsk126@gmail.com>
2024-11-27 20:41:40 -0700

    simple python backend and agent class init

---
+++ backend/agent/agent.py
@@ -0,0 +1,8 @@
+class agent:
+    def __init__(self) -> None:
+        self.context = """"""
+        self.prompt = """"""
+        pass
+
+    def update_prompt(self, prompt: str) -> None:
+        self.prompt = prompt
