1c20377bc38bdee04ff193ad4087fb8d0a51013f
Amul Gaurav <amulgaurav907@gmail.com>
2024-11-29 11:17:10 +0530

    Initialize new branch

--- webapp/app/components/SearchBar.tsx
+++ webapp/app/components/SearchBar.tsx
@@ -11,7 +11,7 @@ const SearcBar = () => {
         type="text"
         id="search-navbar"
         className="bg-[#F8F9FB] sm:bg-white block w-full min-w-48 max-w-72 pr-10 lg:pr-1 xl:pr-16 py-[10px] sm:py-3 ps-10 text-body2-mobile sm:text-body2-desktop font-medium sm:font-bold border rounded-3xl border-gray-600 text-[#404040] placeholder:text-[#D6D6D6] sm:placeholder:text-[#404040] focus:ring-blue-500 focus:border-blue-500"
-        placeholder="Search here"
+        placeholder="Search / here"
       />
     </div>
   );
