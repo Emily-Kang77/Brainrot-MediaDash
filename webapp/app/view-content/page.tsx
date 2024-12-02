c85dbc795297b2f3ab49b96379e778f487ef16cb
Emily-Kang77 <109045956+Emily-Kang77@users.noreply.github.com>
2024-12-01 18:10:31 -0800

    add suspense

--- webapp/app/view-content/page.tsx
+++ webapp/app/view-content/page.tsx
@@ -2,6 +2,7 @@
 
 import ContentView from '@/app/components/ContentView';
 import { Result } from '../types';
+import { Suspense } from 'react';
 
 // Sample data type
 interface Content {
@@ -26,9 +27,11 @@ const sampleContent: Content = {
 export default function ContentPage() {
   return (
     <div className="bg-background-color px-screen-320 lg:px-screen-992 xl:px-screen-1200 2xl:px-screen-1440">
-      <ContentView
-        onClose={() => window.history.back()} // or use router.back()
-      />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ContentView
+          onClose={() => window.history.back()} // or use router.back()
+        />
+      </Suspense>
     </div>
   );
 }
