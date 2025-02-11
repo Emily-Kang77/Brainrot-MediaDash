5a55bf5cdc456c35084ae593bd99d24b432c02da
Emily-Kang77 <emilykang135@gmail.com>
2025-02-10 18:48:39 -0800

    remove sensitive files

---
+++ webapp/app/components/NewContentForm.tsx
@@ -0,0 +1,21 @@
+import { Form } from "@radix-ui/react-form";
+
+export default function NewContentForm() {
+
+    return (
+        <Form>
+
+        </Form>
+        // <Form.Root>
+        //     <Form.Field name="Title">
+        //         <Form.Label>Title</Form.Label>
+        //         <Form.Message className="FormMessage">
+        //             Enter the content's title
+        //         </Form.Message>
+        //         <Form.Control asChild>
+		// 		    <textarea className="Textarea" required />
+		// 	    </Form.Control>
+        //     </Form.Field>
+        // </Form.Root>
+    )
+}
